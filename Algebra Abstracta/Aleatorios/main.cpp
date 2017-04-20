5365b33bbb3f95d3dc96723828290079f569193e
fabiohsm2008 <fabio.soto@ucsp.edu.pe>
2017-04-20 13:45:40 -0500

    Update main.cpp

--- Algebra Abstracta/Aleatorios/main.cpp
+++ Algebra Abstracta/Aleatorios/main.cpp
@@ -2,26 +2,34 @@
 #include <stdlib.h>
 #include <time.h>
 #include <bitset>
+#include <vector>
 
 using namespace std;
 
-int ga(int bits_seed, const int bits_num, int particiones, int vueltas)
+int ga(int bits_seed, int bits_num, int particiones, int vueltas)
 {
     srand(time(NULL));
-    bitset <8> a;
+    bitset <1024> a;
+    vector< bitset<512> > part_n;
+    bitset<512> aux;
+
     for(int i = 0; i < bits_seed; i++){
         a[i + bits_num - bits_seed] = rand()%2;
     }
-    int hola = a.size()-1;
+    int hola = bits_num - 1;
+
     for(int i = bits_num - bits_seed - 1; i >= 0; i--)
     {
         a[i] = a[hola] ^ a[hola - 1];
         hola--;
     }
+
+
+
     cout << a << endl;
 }
 
 int main(){
-    ga(4,8,2,5);
+    ga(30,1024,2,5);
     return 0;
 }
