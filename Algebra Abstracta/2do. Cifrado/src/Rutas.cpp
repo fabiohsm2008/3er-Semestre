f833d2c04ba14c2c3a3f47eef6a2aa4446e42ede
fabiohsm2008 <fabio.soto@ucsp.edu.pe>
2017-04-27 01:45:22 -0500

    Update Rutas.cpp

--- Algebra Abstracta/2do. Cifrado/src/Rutas.cpp
+++ Algebra Abstracta/2do. Cifrado/src/Rutas.cpp
@@ -8,8 +8,38 @@ Rutas::Rutas()
 
 string Rutas::cifrar(string mensaje)
 {
+    int avanza;
+    string encriptado;
 
-    return mensaje;
+    for(int i=0;i<clave;i++)
+    {
+        encriptado += palabra[i];
+        avanza = ((2*(clave-i))-2);
+        while(avanza+i < palabra.size())
+        {
+            if(i== 0){
+                encriptado += palabra[avanza];
+                avanza += ((2*(clave))-2);
+            }else if(i == clave-1)
+            {
+                avanza += (i*2);
+                if(avanza+i > palabra.size())
+                    break;
+                encriptado += palabra[avanza+i];
+            }else if(avanza+i != palabra.size()-1){
+                encriptado += palabra[avanza+i];
+                if(avanza+(i*2)+i < palabra.size()){
+                    avanza += i*2;
+                    encriptado += palabra[avanza+i];
+                }
+                avanza += ((2*(clave-i))-2);
+            }
+            else{
+                avanza += ((2*(clave-i))-2);
+            }
+        }
+    }
+    return encriptado;
 }
 
 string Rutas::descifrar(string mensaje)
