7ea71fcc6fc0f0bdad69950495ab4b89f1bbc111
fabiohsm2008 <fabio.soto@ucsp.edu.pe>
2017-05-05 19:35:59 -0500

    Add files via upload

--- Algebra Abstracta/Aleatorios/main.cpp
+++ Algebra Abstracta/Aleatorios/main.cpp
@@ -6,31 +6,29 @@ using namespace std;
 int main()
 {
 
-    /*srand(time(NULL));
+    srand(time(NULL));
 
-    string texto_plano = "Hola";
+    string texto_plano = "Mango";
     RSA Receptor;
     ZZ clave_publica = Receptor.get_clave_publica();
     ZZ n = Receptor.get_N();
-    RSA Emisor(clave_publica, n);
-    ///RSA Emisor(to_ZZ(175), to_ZZ(30301));
+    ///RSA Emisor(clave_publica, n);
+    /*RSA Emisor(to_ZZ(215), to_ZZ(34717));
     vector <ZZ> texto = Emisor.cifrar(texto_plano);
     for(int i = 0; i < texto.size(); i++){
         cout << texto[i] << ",";
-    }
+    }*/
 
-    int temp[] = {16208,2744,1331,0};
+    int temp[] = {23635,0,15869,13874,22361};
     vector <ZZ> texto;
     for(int i = 0; i < sizeof(temp)/sizeof(temp[0]); i++){
         texto.push_back(to_ZZ(temp[i]));
     }
+    Receptor.set_privada(to_ZZ(13415));
+    Receptor.set_N(to_ZZ(34717));
 
     cout << endl;
 
     texto_plano = Receptor.descifrar(texto);
-    cout << texto_plano << endl;*/
-
-    ///cout << potencia_i(11,7,33);
-    ///cout << inversa(to_ZZ(5), to_ZZ(24));
-
+    cout << texto_plano << endl;
 }
